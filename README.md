29b816205ea2aada0f6e73b3ca12c00aef117e7b
Nadeem <nadeem0035@gmail.com>
2018-07-02 14:56:59 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# allow-html-tag
+Allow html tags in footer copyright text magneto 2
---
+++ registration.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Product:       EijdemsInternet_RedirectCustomer (1.0.0)
+ * ID:            %!uniqueid!%
+ * Packaged:      %!packaged!%
+ * Last Modified: 2018-03-01T19:47:21+00:00
+ * File:          registration.php
+ * Copyright:     Copyright (c) 2018 Eijdems Interne / All rights reserved.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Nadeem0035_AllowHtmlTag',
+    __DIR__
+);
